c8b4d4ad8c084f014af98f3341ecc19d34fc5875
GitHub Action <action@github.com>
2023-11-02 18:13:47 +0000

    deploy

---
+++ assets/3d_blender_rendering.md.9d696428.js
@@ -0,0 +1,1 @@
+import{o as t,c as e}from"./app.81d1f2af.js";const r='{"title":"","description":"","frontmatter":{},"relativePath":"3d/aframe/index.md","lastUpdated":1698948812129}',a={};a.render=function(r,a,d,o,i,n){return t(),e("div")};export default a;export{r as __pageData};
